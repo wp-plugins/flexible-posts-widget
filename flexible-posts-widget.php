058b5fd9e37d87cd6387a9ffc5b5a87658737946
dpe415 <dpe415@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-20 17:46:52 +0000

    Version bump for readme changes.
    git-svn-id: https://plugins.svn.wordpress.org/flexible-posts-widget/trunk@534196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flexible-posts-widget.php
+++ flexible-posts-widget.php
@@ -4,10 +4,7 @@ Plugin Name: Flexible Posts Widget
 Plugin URI: http://wordpress.org/extend/plugins/flexible-posts-widget/
 Author: David Paul Ellenwood
 Author URI: http://dpedesign.com
-Requires at least: 3.2
-Tested up to: 3.3.1
-Stable tag: 1.0.1
-Tags: widget, widgets, posts, recent posts, thumbnails, custom post types, custom taxonomies
+Version: 1.0.1
 Description: An advanced posts display widget with many options: post by taxonomy & term or post type, thumbnails, order & order by, customizable templates
 */
 
