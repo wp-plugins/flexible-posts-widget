06a46057caface391d3d8e3cae9a0e6e7c284757
dpe415 <dpe415@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-16 14:17:58 +0000

    Stylistic layout changes.
    git-svn-id: https://plugins.svn.wordpress.org/flexible-posts-widget/trunk@613219 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/widget.php
+++ views/widget.php
@@ -12,7 +12,9 @@ echo $before_widget;
 if ( !empty($title) )
 	echo $before_title . $title . $after_title;
 
-if( $flexible_posts->have_posts() ): ?>
+if( $flexible_posts->have_posts() ):
+
+?>
 	<ul class="dpe-flexible-posts">
 	<?php while ( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); ?>
 		<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -27,7 +29,7 @@ if( $flexible_posts->have_posts() ): ?>
 	<?php endwhile; ?>
 	</ul><!-- .dpe-flexible-posts -->
 <?php else: // We have no posts ?>
-	<div class="no-posts dpe-flexible-posts">
+	<div class="dpe-flexible-posts no-posts">
 		<p>No post found</p>
 	</div>
 <?php	
