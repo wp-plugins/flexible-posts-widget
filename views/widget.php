7196947af7bb6da5ccaa84f408b84ac630e606a2
dpe415 <dpe415@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-16 14:14:06 +0000

    Corrected a code oder issue in the main widget template file.
    git-svn-id: https://plugins.svn.wordpress.org/flexible-posts-widget/trunk@613217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/widget.php
+++ views/widget.php
@@ -11,29 +11,27 @@ echo $before_widget;
 
 if ( !empty($title) )
 	echo $before_title . $title . $after_title;
-	
-	if( $flexible_posts->have_posts() ):
-?>
-		<ul class="dpe-flexible-posts">
-		<?php while ( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); ?>
-			<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<a href="<?php echo the_permalink(); ?>">
-					<?php
-						if( $thumbnail == true )
-							the_post_thumbnail( $thumbsize );
-					?>
-					<h4 class="title"><?php the_title(); ?></h4>
-				</a>
-			</li>
-		<?php endwhile; ?>
-		</ul><!-- .dpe-flexible-posts -->
-<?php
-		echo $after_widget;
 
-else: ?>
+if( $flexible_posts->have_posts() ): ?>
+	<ul class="dpe-flexible-posts">
+	<?php while ( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); ?>
+		<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<a href="<?php echo the_permalink(); ?>">
+				<?php
+					if( $thumbnail == true )
+						the_post_thumbnail( $thumbsize );
+				?>
+				<h4 class="title"><?php the_title(); ?></h4>
+			</a>
+		</li>
+	<?php endwhile; ?>
+	</ul><!-- .dpe-flexible-posts -->
+<?php else: // We have no posts ?>
 	<div class="no-posts dpe-flexible-posts">
 		<p>No post found</p>
 	</div>
-<?php
-	endif; // End have_posts()
+<?php	
+endif; // End have_posts()
+	
+echo $after_widget;
 ?>
