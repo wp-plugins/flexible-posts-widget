88bf46991cd459027c8badaad4469e170240bf5b
dpe415 <dpe415@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-05 15:22:44 +0000

    Code whitespace changes.
    git-svn-id: https://plugins.svn.wordpress.org/flexible-posts-widget/trunk@621279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/widget.php
+++ views/widget.php
@@ -13,10 +13,9 @@ if ( !empty($title) )
 	echo $before_title . $title . $after_title;
 
 if( $flexible_posts->have_posts() ):
-
 ?>
 	<ul class="dpe-flexible-posts">
-	<?php while ( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); ?>
+	<?php while( $flexible_posts->have_posts() ) : $flexible_posts->the_post(); ?>
 		<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<a href="<?php echo the_permalink(); ?>">
 				<?php
